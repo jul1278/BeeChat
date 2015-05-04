87846c8b77e2f239b7a016d1c21a30c0cab1ec02
jul1278 <jul1278@gmail.com>
2015-05-04 14:23:01 +1000

    Update AppMain.cpp

--- include/AppMain.cpp
+++ include/AppMain.cpp
@@ -1,8 +1,13 @@
 // AppMain.cpp
 
+#include "BeeChatApp.h"
 
 int main( int argc, char* argv )
 {
+  
+  // check arguments does the user want to be a server? otherwise we're a server
+  
+  
   
   
   
