e12b5849a22d6f302583349a16f3c146e39b6886
jul1278 <jul1278@gmail.com>
2015-05-04 14:34:48 +1000

    Update AppMain.cpp

--- include/AppMain.cpp
+++ include/AppMain.cpp
@@ -6,9 +6,12 @@ int main( int argc, char* argv )
 {
   
   // check arguments does the user want to be a server? otherwise we're a server
+  // get requested username from arguments
   
+  // try to be a server if the user wants  
+  BeeChatApp* beeChatApp = new BeeChatApp( string username, userWantsToBeAServer ); 
+
+  while ( beeChatApp->Run() ); 
   
-  
-  
-  
+  return 0;
 }
