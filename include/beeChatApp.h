2b741ab603b2675e3299030908241921d0856c11
jul1278 <jul1278@gmail.com>
2015-05-04 14:05:42 +1000

    Create beeChatApp.h

---
+++ include/beeChatApp.h
@@ -0,0 +1,30 @@
+class BeeChatApp
+{
+private:
+
+
+  // server object
+  Server* server; 
+
+  // client object
+  Client* client
+
+public: 
+
+  BeeChatApp( std::string username )
+  {
+  
+    // start client 
+  
+    // try to start server
+    
+    
+    
+    
+  
+  }
+  
+  
+  ~BeeChatApp(); 
+
+};
