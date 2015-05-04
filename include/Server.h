9b12622d556ce98e2a6a866986dd150cd4d3ca5f
jul1278 <jul1278@gmail.com>
2015-05-04 14:09:31 +1000

    Update Server.h

--- include/Server.h
+++ include/Server.h
@@ -2,16 +2,24 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-// Server
+#include <vector>
 
+// Server
 class Server
 {
 private:
-
+  
+  ServerChatConnection* chatConnection; 
+  
+  std::vector<User> users; 
+  
 public:
   
   Server();
   ~Server(); 
+  
+  void SendMessageToClient(  )
+  Message GetNewMessage(); 
   
 };
 
