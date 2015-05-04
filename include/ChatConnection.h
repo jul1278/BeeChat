5cc426a2f70946649b49ef3639ae7c754d102473
jul1278 <jul1278@gmail.com>
2015-05-04 14:27:26 +1000

    Update ChatConnection.h

--- include/ChatConnection.h
+++ include/ChatConnection.h
@@ -15,11 +15,10 @@ public:
 
 	ChatConnection(); 
 	~ChatConnection(); 
-
-	// what should this return; 
-	void GetLatestClientConnection(); 
 	
-	User GetNewUser(); 
+	// if there's anything on the newUsers queue, return it.
+	User GetNewUserConnection(); 
+
 
 	Message GetLatestMessage(); 
 	void SendMessage( int userID ); 
