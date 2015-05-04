a24cc3bfa1550707041bb752159d57c2c7cfe8a8
jul1278 <jul1278@gmail.com>
2015-05-04 15:19:24 +1000

    Update Server.h

--- include/Server.h
+++ include/Server.h
@@ -11,15 +11,25 @@ private:
   
   ServerChatConnection* chatConnection; 
   
+  // we know about users - each user stores a clientID 
+  // client ID maps to a client address inside chatconnection
   std::vector<User> users; 
+  
+  std::queue<Message> messageQueue; 
   
 public:
   
   Server();
   ~Server(); 
   
-  void SendMessageToClient(  )
-  Message GetNewMessage(); 
+  
+  int Run() 
+  {
+    // have we recieved any messages on the chat connection? 
+    
+    // do we need to relay messages back to clients
+    
+  }
   
 };
 
